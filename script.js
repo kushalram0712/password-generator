c946c14c50b93bde027c7fa9674eba51d6b8b187
BEZAWADA KUSHAL RAM <bkushalram0712@gmail.com>
2026-06-07 15:04:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -10,4 +10,13 @@ function generate() {
     }
     
     document.getElementById('result').innerText = password;
+    document.getElementById('copyStatus').innerText = "";
+}
+
+function copyToClipboard() {
+    const text = document.getElementById('result').innerText;
+    if (text === "---") return;
+    navigator.clipboard.writeText(text).then(() => {
+        document.getElementById('copyStatus').innerText = "Copied to clipboard!";
+    });
 }
